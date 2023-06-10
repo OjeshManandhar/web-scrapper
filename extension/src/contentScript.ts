7f6d20149c9c10a78a9ca2f43d0a30a011ae7aae
OjeshManandhar <ozes.manandhar@gmail.com>
2023-06-10 10:21:36 +0545

    Select elements

--- extension/src/contentScript.ts
+++ extension/src/contentScript.ts
@@ -1,1 +1,51 @@
-console.log('Hello from content-script.ts');
+const selectedElements: HTMLElement[] = [];
+
+// listen for click events on the page
+// save the clicked elements in selectedElements
+document.addEventListener('click', event => {
+  event.preventDefault();
+  const elem = event.target as HTMLElement;
+  if (elem.tagName.toLowerCase() !== 'div') {
+    const isSelected = isElementSelected(elem);
+    if (isSelected) {
+      const index = selectedElements.indexOf(elem);
+      selectedElements.splice(index, 1);
+    } else {
+      selectedElements.push(elem);
+    }
+
+    console.log('selectedElements', selectedElements);
+    console.log('ancestor', findNearestCommonAncestor());
+  }
+});
+
+function isElementSelected(elem: HTMLElement) {
+  return selectedElements.includes(elem);
+}
+
+function findNearestCommonAncestor() {
+  if (selectedElements.length === 0) return null;
+  if (selectedElements.length === 1) return selectedElements[0];
+
+  let ancestor: HTMLElement | null = selectedElements[0];
+
+  while (1) {
+    let foundAncestor = true;
+    if (ancestor === null) return null;
+
+    for (let i = 1; i < selectedElements.length; i++) {
+      const containsNode = ancestor.contains(selectedElements[i]);
+
+      if (!containsNode) {
+        foundAncestor = false;
+        break;
+      }
+    }
+
+    if (foundAncestor) break;
+
+    ancestor = ancestor.parentElement;
+  }
+
+  return ancestor;
+}
