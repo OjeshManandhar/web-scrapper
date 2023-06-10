f18762043524c5c4a3dc7a99fe877757c748b2be
OjeshManandhar <ozes.manandhar@gmail.com>
2023-06-10 07:50:14 +0545

    Basic working

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   mode: 'production',
   entry: {
-    background: path.resolve(__dirname, '..', 'src', 'background.ts'),
+    background: path.resolve(__dirname, 'src', 'background.ts'),
   },
   output: {
-    path: path.join(__dirname, '../dist'),
+    path: path.join(__dirname, 'dist'),
     filename: '[name].js',
   },
   resolve: {
